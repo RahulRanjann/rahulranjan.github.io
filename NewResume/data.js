474d603810811d866d7ac049999400df102aa96a
rahulranjann <rahul1913111054@gmail.com>
2024-02-29 19:08:55 +0530

    data.js and Script.js successfully added

--- NewResume/data.js
+++ NewResume/data.js
@@ -5,24 +5,55 @@ const profileData = {
         src: "some",
         alt: "me",
     },
-    education: [
+
+    internship: [
         {
-            name: "Manipal Institute Of Technology, Manipal",
-            board: "MAHE",
-            duration: "2022 to present",
-            std: "B.Tech. in Compute Science"
+            name: "Pine&Lime",
+            duration: "Jan 2024 to Present",
+            address: "Gurgaon, Haryana, IN",
+            role: "Full Stack Developer",
+            workMode: "Remote",
+            desc:[
+                "Develop and maintain responsive web applications using Wix, HTML, CSS, JavaScript, and Git.",
+                "Contribute to both frontend and backend development, ensuring seamless user experiences."
+            ]
+
         },
         {
-            name: "Rajokari Institute Of Technology, Delhi",
+            name: "GoMile Stone",
+            duration: "Dec 2023",
+            address: "Gurgaon, Haryana, IN",
+            role: "Software Developer",
+            workMode: "On Site",
+            desc:[
+                "Develop and maintain responsive web applications using Wix, HTML, CSS, JavaScript, and Git.",
+                "Contribute to both frontend and backend development, ensuring seamless user experiences."
+            ]
+        }
+    ],
+
+    education: [
+        {
+            name: "Manipal Institute Of Technology",
+            board: "MAHE",
+            duration: "2022 to present",
+            std: "B.Tech. in Compute Science",
+            address: "Udipi, IN"
+        },
+        {
+            name: "Rajokari Institute Of Technology",
             board: "BTE Delhi",
             duration: "2019 to 2022",
-            std: "Diploma in Information Technology"
+            std: "Diploma in Information Technology",
+            address: "Rajokari, IN"
+
         },
         {
             name: "Boodha Woodland Public School",
             board: "CBSE",
             duration: "2017 to 2019",
-            std: "Secondary School"
+            std: "Secondary School",
+            address: "Bihar, IN"
         }
     ],
     skill: [
--- NewResume/script.js
+++ NewResume/script.js
@@ -1,23 +1,66 @@
 function setEducation(education) {
-    const eduList = document.querySelector(".educationList");
+    const eduList = document.querySelector(".education");
     education.forEach((edu)=>{
         eduList.innerHTML += `
-            <li >
-                <div class="collage">
-                    <div >
-                        <h4>${edu.name}</h4>
-                        <ul>
-                            <h5>${edu.board}</h5>
-                            <h5>${edu.std}</h5>
-                        </ul>
-                    </div>
-                    <div >
-                        <div class="date">${edu.duration}</div>
-                    </div>
+            <div class="institute">
+            <div class="instituteHead">
+                <div class="instituteName">
+                    <p class="">
+                        ${edu.name}
+                    </p>
                 </div>
-            </li>
+                <div class="date">
+                    <p>
+                    ${edu.duration}
+                    </p>
+                </div>
+            </div>
+            <div class="instituteLocation">
+                <p class="location">${edu.address}</p>
+            </div>
+            <div class="role">
+                <p>
+                    <ul>
+                        <li>${edu.std}</li>
+                    </ul>
+                </p>
+            </div>
+        </div>
         `
     });
+}
+
+function setInternship(internship){
+    const  Internship = document.querySelector(".company");
+    internship.forEach((i)=>{
+        Internship.innerHTML += `
+        <div class="companyHead">
+            <div class="companyName">
+                <p class="">
+                    ${i.name}
+                </p>
+            </div>
+            <div class="date">
+                <p>
+                    ${i.duration}
+                </p>
+            </div>
+        </div>
+        <div class="workFrom">
+            <p class="companyLocation">${i.address}</p>
+            <div class="workLocation">
+                <p>${i.workMode}</p>
+            </div>
+        </div>
+        <div class="role">
+                <ul>
+                    ${i.desc.map(d => `<li>${d}</li>`).join('')}
+                </ul>
+        </div>
+    
+        `
+    })
+
 }
 
 function setSkill(skill){
@@ -53,6 +96,7 @@ function setImg(img){
 
 (()=>{
     setEducation(profileData.education);
+    setInternship(profileData.internship);
     setSkill(profileData.skill);
     setName(profileData.name);
     setSubTitle(profileData.subtitle);
